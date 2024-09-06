fc07e9209de8efd86c0b3c4c60df58b518f53ca4
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-09-06 14:16:36 -0300

    feat: commit inicial

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'comma-dangle': ['error', 'always-multiline'],
+    'multiline-ternary': 0,
+    '@typescript-eslint/no-empty-interface': 'off',
+
+    'react/prop-types': [0],
+    'react/display-name': 'off',
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+  },
+}
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  printWidth: 100,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  useTabs: false,
+  tabWidth: 2,
+}
