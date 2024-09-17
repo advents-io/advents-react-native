64a1c1d958f5b1460cca1aad773a329478e8541d
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-09-17 16:45:21 -0300

    chore: add eslint and prettier ignores

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  ignorePatterns: ['node_modules', 'pnpm-lock.yaml', 'dist', 'examples'],
   extends: [
     'plugin:react/recommended',
     'standard',
