312bdcc26f46836c6c82001ec60112576ec84daa
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-11-27 15:06:00 -0300

    chore: remove examples

--- scripts/use-build.sh
+++
@@ -1,7 +0,0 @@
-rm -rf examples/expo-test/advents-react-native
-rm -rf examples/react-native-test/advents-react-native
-pnpm build
-mkdir -p examples/expo-test/advents-react-native
-mkdir -p examples/react-native-test/advents-react-native
-cp -R dist/* examples/expo-test/advents-react-native
-cp -R dist/* examples/react-native-test/advents-react-native
